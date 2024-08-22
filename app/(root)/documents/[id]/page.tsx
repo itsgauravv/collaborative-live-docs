77128c14295c696ca5a1c5dcd6ad61501b6cc650
itsgauravv <gauravjani3113@gmail.com>
2024-08-22 06:53:03 +0530

    init commit + changes

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,11 @@
+import CollaborativeRooms from "@/components/CollaborativeRooms";
+
+const Document = () => {
+  return (
+    <main className="flex w-full flex-col items-center">
+      <CollaborativeRooms />
+    </main>
+  );
+};
+
+export default Document;
